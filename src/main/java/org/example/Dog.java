5e3fc431c6933cbd844854c80ee17a6fd62e5c7b
Nicolò Pandolfo <nicolo.pandolfo@icloud.com>
2023-10-23 23:01:23 +0200

    secondaversione

---
+++ src/main/java/org/example/Dog.java
@@ -0,0 +1,47 @@
+package org.example;
+
+public class Dog {
+
+    // Dichiarazione variabili locali
+    private String name;
+    private int age;
+
+    // Costruttore classe
+    public Dog(){
+    }
+
+    public Dog(String name) {
+        this.name = name;
+    }
+
+    public Dog(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Metodo per restituire name
+    public String getName(){
+        return name;
+    }
+    // Metodo per restituire age
+    public int getAge(){
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Metodo per restituire proprietà oggetto
+    public String showProperties(){
+        return "This "
+                + getAge()
+                + " year old dog loves to be called "
+                + getName()
+                + ".";
+    }
+}
